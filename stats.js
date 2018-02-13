022eacd2efdb44276730c765e5cfa69053fabe2a
Radhika Mattoo <rm3485@nyu.edu>
2018-02-13 17:00:26 -0500

    chained promises still not resolving

--- stats.js
+++ stats.js
@@ -17,53 +17,50 @@ const dbRef = db.ref("/gameBuilder");
 const outfile = "statistics.json";
 let json = {}; // "{Monopoly: { cloudStoragePath : ____, metadata: {originalSize:__, quality70:__, quality70:__} }"
 
-return dbRef.once("value").then((gameBuilder) => {
-  const imagesToDownload = []; // [{cloudPath:___, destinationPath:___}]
-  const specs = gameBuilder.child('gameSpecs');
-  const elements = gameBuilder.child('elements');
-  const images = gameBuilder.child('images');
-  const promises = [];
-  let count = 0;
-  specs.forEach((gameSpec) => { //for each gameSpec object get all ids for the pieces
-    count += 1;
-    const elementIds = [];
-    const imageIds = [];
-    const gameName = gameSpec.child('gameName').val();
-    console.log("GAME: " + gameName + " count: ", count);
-    json[gameName] = {};
-
-    const pieces = gameSpec.child('pieces');
-    pieces.forEach((piece) => {
-      const elementId = piece.child('pieceElementId').val();
-      elementIds.push(elementId);
-    });
-
-    // Get imageId for each element in the game
-    elementIds.forEach((elementId) =>{
-      const imageRefs = elements.child(elementId).child('images');
-      imageRefs.forEach((ref) =>{
-        imageIds.push( ref.child('imageId').val() );
+function getImageData(){
+  return dbRef.once("value").then((gameBuilder) => {
+    const specs = gameBuilder.child('gameSpecs');
+    const elements = gameBuilder.child('elements');
+    const images = gameBuilder.child('images');
+    specs.forEach((gameSpec) => {
+      const promises = [];
+       //for each gameSpec object get its image data
+      const elementIds = [];
+      const imageIds = [];
+      const gameName = gameSpec.child('gameName').val();
+      json[gameName] = {};
+      console.log("Game is: ", gameName);
+      const pieces = gameSpec.child('pieces');
+      // for each spec's piece, get its elementId
+      pieces.forEach((piece) => {
+        const elementId = piece.child('pieceElementId').val();
+        elementIds.push(elementId);
       });
-    });
 
-    // get the image cloud path from imageIds
-    imageIds.forEach((imageId) =>{
-      const isBoard = images.child(imageId).child('isBoardImage').val();
-      console.log(isBoard);
-      if(isBoard){
+      //for each elementId get its imageIDs
+      elementIds.forEach((elementId) =>{
+        const imageRefs = elements.child(elementId).child('images');
+        imageRefs.forEach((ref) =>{
+          imageIds.push( ref.child('imageId').val() );
+        });
+      });
+
+      ///for each imageId get its cloudStoragePath and perform conversions
+      imageIds.forEach((imageId) =>{
+        const isBoard = images.child(imageId).child('isBoardImage').val();
         const cloudPath = images.child(imageId).child('cloudStoragePath').val(); //images/dog.jpg
         const obj = {'cloudStoragePath' : cloudPath, 'metadata': {} };
         json[gameName] = obj;
-        console.log(cloudPath);
-        console.log(json[gameName]);
+        const JPEG_EXTENSION = '.jpg';
         const filename = cloudPath.split("/")[1]; //dog.jpg
-        let file, extension = filename.split("."); // dog, jpg
-        const thumbFileName = file + '.jpg'; // dog.jpg
+        const file = filename.split(".")[0]; // dog
+        const extension = filename.split(".")[1];
+        const thumbFileName = file + JPEG_EXTENSION; // dog.jpg
 
         // Temporary paths for images
         const tempDir = os.tmpdir(); //tmp
         const tempFilePath = path.join(tempDir, filename); // tmp/dog.jpg
-        const tempThumb = path.join(tempDir, file + "_thumb.jpg"); //tmp/dog_thumb.jpg
+        const tempThumb = path.join(tempDir,  path.format( { name: file + "_thumb" , ext: JPEG_EXTENSION}) ); //tmp/dog_thumb.jpg
         const temp50 = path.join(tempDir, path.format( { name: file + "_50" , ext: extension} )); // /tmp/dog_50.jpg
         const temp70 = path.join(tempDir, path.format( { name: file + "_70" , ext: extension} )); // /tmp/dog_70.jpg
 
@@ -73,61 +70,64 @@ return dbRef.once("value").then((gameBuilder) => {
         const thumbnailPath = path.join('images/thumbnail', thumbFileName);
         const fileRef = storage.file(cloudPath);
 
-        console.log("Resizing the image with path: ", filePath, ", name: ", filename, " and extension: ", extension);
-        console.log("Download path: ", tempFilePath);
-        console.log("Temp 50 path: ", temp50);
-        console.log("Temp 70 path: ", temp70);
-        console.log("Temp thumb path: ", tempThumb);
-        console.log("File50 GCS path: ", filePath50);
-        console.log("File70 GCS path: ", filePath70);
-        console.log("Thumbnail GCS path: ", thumbnailPath);
+        // console.log("Resizing the image with path: ", cloudPath, ", name: ", filename, " and extension: ", extension);
 
-        let promise = fileRef.getMetadata().then((results) =>{
-          const metadata = results[0];
-          console.log("Retrieved metadata, original file size is: ", metadata.size);
-          json[gameName]['metadata']['originalSize'] = metadata.size;
-          return fileRef.download({
+        let promise = fileRef.download({
             destination: tempFilePath
-          });
         }).then(() => {
-          console.log('Image downloaded locally to', tempFilePath);
-          console.log("Converting image to quality70");
+          json[gameName]['metadata']['original'] = getFileSize(tempFilePath);
+          // console.log('Image downloaded locally to', tempFilePath);
+          // console.log("Converting image to quality70");
           return spawn('convert', [tempFilePath, '-quality', '70', temp70]);
         }).then(() => {
-          console.log("Uploading quality70 image");
+          json[gameName]['metadata']['quality70'] = getFileSize(temp70);
+          // console.log("Uploading quality70 image");
           return storage.upload(temp70, { destination: filePath70 });
         }).then(()=>{
-          console.log("Converting image to quality50");
+          // console.log("Converting image to quality50");
           return spawn('convert', [tempFilePath, '-quality', '50', temp50]);
         }).then(()=>{
-          console.log("Uploading quality50 image");
+          json[gameName]['metadata']['quality50'] = getFileSize(temp70);
+          // console.log("Uploading quality50 image");
           return storage.upload(temp50, { destination: filePath50});
         }).then(()=>{
-          console.log('Converting image to thumbnail');
+          // console.log('Converting image to thumbnail');
           return spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempThumb]);
         }).then(()=>{
-          console.log("Uploading thumbnail");
+          // console.log("Uploading thumbnail");
           return storage.upload(tempThumb, { destination: thumbnailPath });
         }).then(() =>{
-          console.log("Unlinking temp files");
+          // console.log("Unlinking temp files");
           fs.unlinkSync(tempFilePath);
           fs.unlinkSync(tempThumb);
           fs.unlinkSync(temp70);
           fs.unlinkSync(temp50);
           return true;
-          // console.log("EXITING");
-          // process.exit(0);
+        }).catch(err =>{
+          return true;
         });
-        promises.push(promise);
-      }
-    }); //imageIds forEach
+        return Promise.all([promise]);
 
-  }); //specs forEach
-  return Promise.all(promises);
-});
-
-function getFilesizeInBytes(filename) {
-    const stats = fs.statSync(filename)
-    const fileSizeInBytes = stats.size
-    return fileSizeInBytes
+      }); //imageIds forEach
+    }); //specs forEach
+  }).catch(err =>{  //db .once
+    console.log(err);
+    return;
+  });
 }
+
+// Taken from: https://gist.github.com/narainsagar/5cfd315ab38ba363191b63f8ae8b27db
+function getFileSize(filePath) {
+  var stats = fs.statSync(filePath);
+  // console.log('stats', stats);
+  var size = stats["size"];
+  // convert it to humanly readable format.
+  var i = Math.floor( Math.log(size) / Math.log(1024) );
+  return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
+}
+
+getImageData().then(() =>{
+  console.log(json);
+  fs.writeFileSync(outfile, JSON.stringify(json));
+  return admin.app().delete();
+})
